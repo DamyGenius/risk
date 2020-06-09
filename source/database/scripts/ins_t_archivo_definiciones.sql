52aa2b76899d439ab66ceb06759df7ad980085c5
jtsoya539 <javier.meza.py@gmail.com>
2020-06-09 13:18:23 -0400

    Agregar logos de divisiones y escudos de clubes

--- source/database/scripts/ins_t_archivo_definiciones.sql
+++ source/database/scripts/ins_t_archivo_definiciones.sql
@@ -1,6 +1,13 @@
 prompt Importing table t_archivo_definiciones...
 set feedback off
 set define off
+
+insert into t_archivo_definiciones (TABLA, CAMPO, DESCRIPCION, TAMANO_MAXIMO, ORDEN, NOMBRE_REFERENCIA, EXTENSIONES_PERMITIDAS)
+values ('T_CLUBES', 'ESCUDO', 'Escudo del club', 10000000, 1, 'ID_CLUB', 'EXTENSION_IMAGEN');
+
+insert into t_archivo_definiciones (TABLA, CAMPO, DESCRIPCION, TAMANO_MAXIMO, ORDEN, NOMBRE_REFERENCIA, EXTENSIONES_PERMITIDAS)
+values ('T_DIVISIONES', 'LOGO', 'Logo de la división', 10000000, 1, 'ID_DIVISION', 'EXTENSION_IMAGEN');
+
 insert into t_archivo_definiciones (TABLA, CAMPO, DESCRIPCION, TAMANO_MAXIMO, ORDEN, NOMBRE_REFERENCIA, EXTENSIONES_PERMITIDAS)
 values ('T_USUARIOS', 'AVATAR', 'Avatar del usuario (imagen de perfil)', 10000000, 1, 'ALIAS', 'EXTENSION_IMAGEN');
 
