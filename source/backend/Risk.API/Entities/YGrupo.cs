ee5debfc8f581c85d97646fa3abc5d059583e5b1
jtsoya539 <javier.meza.py@gmail.com>
2020-08-06 20:03:11 -0400

    Agregar entities y models para grupos de predicciones

---
+++ source/backend/Risk.API/Entities/YGrupo.cs
@@ -0,0 +1,58 @@
+/*
+--------------------------------- MIT License ---------------------------------
+Copyright (c) 2019 jtsoya539
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+-------------------------------------------------------------------------------
+*/
+
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Risk.API.Entities
+{
+    public class YGrupo
+    {
+        [JsonProperty("id_grupo")]
+        public int IdGrupo { get; set; }
+        [JsonProperty("id_torneo")]
+        public string IdTorneo { get; set; }
+        [JsonProperty("descripcion")]
+        public string Descripcion { get; set; }
+        [JsonProperty("tipo")]
+        public string Tipo { get; set; }
+        [JsonProperty("id_usuario_administrador")]
+        public int IdUsuarioAdministrador { get; set; }
+        [JsonProperty("fecha_creacion")]
+        public DateTime? FechaCreacion { get; set; }
+        [JsonProperty("id_jornada_inicio")]
+        public int IdJornadaInicio { get; set; }
+        [JsonProperty("estado")]
+        public string Estado { get; set; }
+        [JsonProperty("situacion")]
+        public string Situacion { get; set; }
+        [JsonProperty("id_club")]
+        public string IdClub { get; set; }
+        [JsonProperty("todos_invitan")]
+        public string TodosInvitan { get; set; }
+        [JsonProperty("usuarios")]
+        public List<YGrupoUsuario> Usuarios { get; set; }
+    }
+}
