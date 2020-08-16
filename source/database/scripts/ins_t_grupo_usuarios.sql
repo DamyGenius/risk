b17ba67ec91fc2053e5f1bde7a72142d7ca82a0e
dmezac <damian.meza.py@hotmail.com>
2020-08-16 13:52:14 -0400

    Actualización de estado registrado para Demo de invitación pendiente a grupo.-

--- source/database/scripts/ins_t_grupo_usuarios.sql
+++ source/database/scripts/ins_t_grupo_usuarios.sql
@@ -24,6 +24,6 @@ insert into t_grupo_usuarios (ID_GRUPO, ID_USUARIO, PUNTOS, RANKING, ESTADO, TOK
 values ((select id_grupo
           from t_grupos
          where descripcion = 'Grupo Privado 2'
-           and id_torneo = 'PRI-DEMO'), 1, 6, 1, 'A', null, 'N');
+           and id_torneo = 'PRI-DEMO'), 1, 6, 1, 'P', null, 'N');
 
 prompt Done.
