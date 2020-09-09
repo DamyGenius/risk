7be90335004b7d0a11795ea82af9d07ea6866645
jtsoya539 <javier.meza.py@gmail.com>
2020-09-08 20:01:08 -0400

    Renombrar paquete k_planificador por k_trabajo

--- source/database/jobs/create_actualizacion_partidos.sql
+++ source/database/jobs/create_actualizacion_partidos.sql
@@ -26,9 +26,9 @@ set serveroutput on size unlimited
 
 BEGIN
   -- Crear el trabajo diario de actualización de partidos
-  k_planificador.p_crear_o_editar_trabajo(i_id_trabajo   => k_planificador.c_actualizacion_partidos,
-                                          i_fecha_inicio => trunc(current_timestamp) + 0.5 --hoy a las 12hs
-                                          );
+  k_trabajo.p_crear_o_editar_trabajo(i_id_trabajo   => k_trabajo.c_actualizacion_partidos,
+                                     i_fecha_inicio => trunc(current_timestamp) + 0.5 --hoy a las 12hs
+                                     );
 END;
 /
 
