cbed11913e9592b193f640d5866947fd018ad69d
dmezac <damian.meza.py@hotmail.com>
2021-06-22 19:45:45 -0400

    Configuración de plantillas de notificaciones push de Fantasy.-

--- source/database/modules/msj/scripts/ins_t_notificacion_plantillas.sql
+++ source/database/modules/msj/scripts/ins_t_notificacion_plantillas.sql
@@ -2,7 +2,10 @@ prompt Importing table t_notificacion_plantillas...
 set feedback off
 set define off
 insert into t_notificacion_plantillas (ID_APLICACION, ID_PLANTILLA, NOMBRE, ACTIVO, DETALLE, PLANTILLA)
-values ('AND', 'GEN', 'GENERAL', 'S', 'Notificaciones generales para la plataforma Android', '{"notification":{"title":"$(titulo)","body":"$(contenido)","image":"https://image.freepik.com/foto-gratis/pelota-futbol-campo-futbol-hierba-verde_34263-98.jpg"},"data":{"title":"$(titulo)","body":"$(contenido)"}}');
+values ('AND', 'GEN', 'GENERAL', 'S', 'Notificaciones generales para la plataforma Android', '{"data":{"title":"$(titulo)","body":"$(contenido)","image":"https://image.freepik.com/foto-gratis/pelota-futbol-campo-futbol-hierba-verde_34263-98.jpg"}}');
+
+insert into t_notificacion_plantillas (ID_APLICACION, ID_PLANTILLA, NOMBRE, ACTIVO, DETALLE, PLANTILLA)
+values ('AND', 'MSJ', 'MENSAJERIA', 'S', 'Notificaciones de mensajería para la plataforma Android', '{"data":{"type":"$(tipo)","group_id":"$(id_grupo)","group_name":"$(nombre_grupo)","sender":"$(usuario)","message":"$(mensaje)"}}');
 
 insert into t_notificacion_plantillas (ID_APLICACION, ID_PLANTILLA, NOMBRE, ACTIVO, DETALLE, PLANTILLA)
 values ('IOS', 'GEN', 'GENERAL', 'S', 'Notificaciones generales para la plataforma iOS', '{"aps":{"alert":"$(contenido)"}}');
