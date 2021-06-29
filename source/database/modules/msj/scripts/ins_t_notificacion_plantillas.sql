f8152f5577df1cdb79fe5b864a919ff6795afc97
dmezac <damian.meza.py@hotmail.com>
2021-06-28 22:39:18 -0400

    Agregar notificación push en solicitud de amistad e invitación a grupo.-

--- source/database/modules/msj/scripts/ins_t_notificacion_plantillas.sql
+++ source/database/modules/msj/scripts/ins_t_notificacion_plantillas.sql
@@ -5,6 +5,9 @@ insert into t_notificacion_plantillas (ID_APLICACION, ID_PLANTILLA, NOMBRE, ACTI
 values ('AND', 'GEN', 'GENERAL', 'S', 'Notificaciones generales para la plataforma Android', '{"data":{"title":"$(titulo)","body":"$(contenido)","image":"https://image.freepik.com/foto-gratis/pelota-futbol-campo-futbol-hierba-verde_34263-98.jpg"}}');
 
 insert into t_notificacion_plantillas (ID_APLICACION, ID_PLANTILLA, NOMBRE, ACTIVO, DETALLE, PLANTILLA)
+values ('AND', 'ESP', 'ESPECIAL', 'S', 'Notificaciones especiales para la plataforma Android', '{"data":{"title":"$(titulo)","body":"$(contenido)","type":"$(tipo)","id":"$(identificador)"}}');
+
+insert into t_notificacion_plantillas (ID_APLICACION, ID_PLANTILLA, NOMBRE, ACTIVO, DETALLE, PLANTILLA)
 values ('AND', 'MSJ', 'MENSAJERIA', 'S', 'Notificaciones de mensajería para la plataforma Android', '{"data":{"type":"$(tipo)","group_id":"$(id_grupo)","group_name":"$(nombre_grupo)","sender":"$(usuario)","message":"$(mensaje)"}}');
 
 insert into t_notificacion_plantillas (ID_APLICACION, ID_PLANTILLA, NOMBRE, ACTIVO, DETALLE, PLANTILLA)
