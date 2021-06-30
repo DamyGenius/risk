492a9895633f4c2a0bda56e2d7b3ad779e97bf54
jtsoya539 <javier.meza.py@gmail.com>
2021-06-29 21:40:42 -0400

    Agregar dominio FAN en módulo RISK

--- source/database/scripts/ins_t_dominios.sql
+++ source/database/scripts/ins_t_dominios.sql
@@ -1,6 +1,10 @@
 prompt Importing table t_dominios...
 set feedback off
 set define off
+
+insert into t_dominios (ID_DOMINIO, NOMBRE, DETALLE, ACTIVO, ID_MODULO)
+values ('FAN', 'FANTASY', null, 'S', 'RISK');
+
 insert into t_dominios (ID_DOMINIO, NOMBRE, DETALLE, ACTIVO, ID_MODULO)
 values ('GEN', 'GENERAL', null, 'S', 'RISK');
 
