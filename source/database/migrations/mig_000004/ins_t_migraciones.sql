8226babcaa43410a6cce9aa7f37d4af180ec9021
dmezac <damian.meza.py@hotmail.com>
2021-07-18 21:30:23 -0400

    Importación de datos de un partido en particular.-

---
+++ source/database/migrations/mig_000004/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000004', 'Importación de datos de un partido en particular.');
+
+prompt Done.
