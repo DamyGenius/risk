cb8afd0f66e396501c6d1894778e7b77d005de36
dmezac <damian.meza.py@hotmail.com>
2021-07-30 19:28:08 -0400

    Migración 12: Nueva notificación de puntos realizados al finalizar el partido.-

---
+++ source/database/migrations/mig_000012/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000012', 'Migración 11: Nueva notificación de puntos realizados al finalizar el partido.-');
+
+prompt Done.
