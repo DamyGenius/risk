764b2be10bd480d2acd22223abe1842b00f2c3e5
dmezac <damian.meza.py@hotmail.com>
2021-08-16 21:40:20 -0400

    Migración 19: Cantidad de predicciones de partidos.-

---
+++ source/database/migrations/mig_000019/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000019', 'Cantidad de predicciones de partidos.');
+
+prompt Done.
