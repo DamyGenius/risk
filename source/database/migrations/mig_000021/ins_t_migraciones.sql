633f47109600bb50ef5fd8931ff836066a07785f
dmezac <damian.meza.py@hotmail.com>
2021-08-17 21:47:30 -0400

    Migración 21: Retornar club favorito en datos del usuario.-

---
+++ source/database/migrations/mig_000021/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000021', 'Retornar club favorito en datos del usuario.');
+
+prompt Done.
