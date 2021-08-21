318752cd19dc65846f9f4d59b44557ab28edd5a9
dmezac <damian.meza.py@hotmail.com>
2021-08-20 23:53:20 -0400

    Migración 22: Permitir programación manual de partidos.-

---
+++ source/database/migrations/mig_000022/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000022', 'Permitir programación manual de partidos.');
+
+prompt Done.
