6fa1d6e14a39dfc6196f3b44599fa94f23d3a28d
dmezac <damian.meza.py@hotmail.com>
2021-08-22 15:14:01 -0400

    Migración 23: Sugerir calificación de app después de cierta cantidad de jornadas.-

---
+++ source/database/migrations/mig_000023/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000023', 'Sugerir calificación de app después de cierta cantidad de jornadas.');
+
+prompt Done.
