ef03dd366fd1199d51cd8cfed161f8e6375f8b31
dmezac <damian.meza.py@hotmail.com>
2021-08-22 16:02:17 -0400

    Migración 24: Editar dato extra de usuario.-

---
+++ source/database/migrations/mig_000024/ins_t_operaciones.sql
@@ -0,0 +1,7 @@
+﻿prompt Importing table t_operaciones...
+set feedback off
+set define off
+insert into t_operaciones (ID_OPERACION, TIPO, NOMBRE, DOMINIO, ACTIVO, DETALLE, VERSION_ACTUAL, NIVEL_LOG, PARAMETROS_AUTOMATICOS)
+values (70, 'S', 'EDITAR_DATO_USUARIO', 'AUT', 'S', null, '0.1.0', 1, null);
+
+prompt Done.
---
+++ source/database/migrations/mig_000024/ins_t_servicios.sql
@@ -0,0 +1,7 @@
+﻿prompt Importing table T_SERVICIOS...
+set feedback off
+set define off
+insert into T_SERVICIOS (ID_SERVICIO, TIPO, CANTIDAD_EJECUCIONES, FECHA_ULTIMA_EJECUCION, CONSULTA_SQL, SQL_ULTIMA_EJECUCION)
+values (70, 'T', null, null, null, null);
+
+prompt Done.
