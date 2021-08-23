c7f7a627854d48cacd518ce0c3a8428af3d41aa4
dmezac <damian.meza.py@hotmail.com>
2021-08-22 21:28:01 -0400

    Migración 26: Acción de calificar aplicación de Android.-

--- source/database/migrations/mig_000026/ins_t_dato_definiciones.sql
+++ source/database/migrations/mig_000026/ins_t_dato_definiciones.sql
@@ -1,10 +1,6 @@
-prompt Importing table t_dato_definiciones...
+﻿prompt Importing table t_dato_definiciones...
 set feedback off
 set define off
-
-insert into t_dato_definiciones (TABLA, CAMPO, DESCRIPCION, ORDEN, NOMBRE_REFERENCIA, TIPO_DATO)
-values ('T_USUARIOS', 'ID_CLUB', 'Club favorito del usuario', 1, 'ALIAS', 'S');
-
 insert into t_dato_definiciones (TABLA, CAMPO, DESCRIPCION, ORDEN, NOMBRE_REFERENCIA, TIPO_DATO)
 values ('T_USUARIOS', 'CALIFICA_AND', 'Usuario califica aplicación de Android', 2, 'ALIAS', 'S');
 
