6897cb1878f51180dec36665ce1e6690909c182e
dmezac <damian.meza.py@hotmail.com>
2021-10-10 11:36:58 -0300

    Migración 47: Ajustes al obtener parámetros en listar_clubes.-

---
+++ source/database/migrations/mig_000047/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000047', 'Ajustes al obtener parámetros en listar_clubes.');
+
+prompt Done.
