b1694bf18ed785381af6c4b2b7f9968cea712db3
dmezac <damian.meza.py@hotmail.com>
2021-10-10 23:39:12 -0300

    Migración 48: Mejora en editar_dato_usuario.-

---
+++ source/database/migrations/mig_000048/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000048', 'Mejora en editar_dato_usuario.');
+
+prompt Done.
