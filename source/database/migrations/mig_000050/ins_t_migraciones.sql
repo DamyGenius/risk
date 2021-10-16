abc7112a0da4cd4bdd714e7f4732bd5a383eb646
dmezac <damian.meza.py@hotmail.com>
2021-10-16 18:32:24 -0300

    Migración 50: Mejora en notificación push a usuario.-

---
+++ source/database/migrations/mig_000050/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000050', 'Mejora en notificación push a usuario.');
+
+prompt Done.
