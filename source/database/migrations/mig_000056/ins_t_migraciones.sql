8f641fe9e7d612e57646a2e866a5becd073061fe
dmezac <damian.meza.py@hotmail.com>
2022-01-10 21:34:19 -0300

    Migración 56: Dinamizar inicio de jornada de torneo por división.-

---
+++ source/database/migrations/mig_000056/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000056', 'Dinamizar torneo a notificar inicio de jornada por división.');
+
+prompt Done.
