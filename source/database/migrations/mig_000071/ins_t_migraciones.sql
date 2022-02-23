61aedfc4f9b5d530758ce3d74aa6f43e22864d04
dmezac <damian.meza.py@hotmail.com>
2022-02-22 21:37:21 -0300

    Migración 71: Ajuste de texto.

--- source/database/migrations/mig_000071/ins_t_migraciones.sql
+++ source/database/migrations/mig_000071/ins_t_migraciones.sql
@@ -2,6 +2,6 @@ prompt Importing table t_migraciones...
 set feedback off
 set define off
 insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
-values ('mig_000071', 'Grupos por división y torneo (3ra parte).');
+values ('mig_000071', 'Grupos por división y torneo (3ra parte): Estado de Usuarios por Grupo.');
 
 prompt Done.
