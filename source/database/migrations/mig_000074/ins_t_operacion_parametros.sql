d8e74919520473ec3b5aa11facccc618fdacd475
dmezac <damian.meza.py@hotmail.com>
2022-03-12 09:06:26 -0300

    Migración 74: Recibir parámetro de contexto token_dispositivo en la Base de Datos.-

---
+++ source/database/migrations/mig_000074/ins_t_operacion_parametros.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_operacion_parametros...
+set feedback off
+set define off
+insert into t_operacion_parametros (ID_OPERACION, NOMBRE, VERSION, ORDEN, ACTIVO, TIPO_DATO, FORMATO, LONGITUD_MAXIMA, OBLIGATORIO, VALOR_DEFECTO, ETIQUETA, DETALLE, VALORES_POSIBLES)
+values (1001, 'TOKEN_DISPOSITIVO', '0.1.0', 5, 'S', 'S', null, null, 'N', null, null, null, null);
+
+prompt Done.
--- source/database/scripts/operations/parametros/_/contexto.sql
+++ source/database/scripts/operations/parametros/_/contexto.sql
@@ -16,11 +16,11 @@ begin
   l_clob(3) :=q'!CONTEXTO!';
   l_clob(4) :=q'!!';
   l_clob(5) :=q'!S!';
-  l_clob(6) :=q'!Par√°metros de contexto de la ejecuci√≥n!';
+  l_clob(6) :=q'!Par·metros de contexto de la ejecuciÛn!';
   l_clob(7) :=q'!0.1.0!';
   l_varchar2(8) :=q'!0!';
   l_clob(9) :=q'!!';
-
+
   insert into t_operaciones
   (
      "ID_OPERACION"
@@ -74,7 +74,7 @@ begin
   l_clob(11) :=q'!!';
   l_clob(12) :=q'!!';
   l_clob(13) :=q'!!';
-
+
   insert into t_operacion_parametros
   (
      "ID_OPERACION"
@@ -121,7 +121,7 @@ begin
   l_clob(11) :=q'!!';
   l_clob(12) :=q'!!';
   l_clob(13) :=q'!!';
-
+
   insert into t_operacion_parametros
   (
      "ID_OPERACION"
@@ -168,7 +168,7 @@ begin
   l_clob(11) :=q'!!';
   l_clob(12) :=q'!!';
   l_clob(13) :=q'!!';
-
+
   insert into t_operacion_parametros
   (
      "ID_OPERACION"
@@ -215,7 +215,54 @@ begin
   l_clob(11) :=q'!!';
   l_clob(12) :=q'!!';
   l_clob(13) :=q'!!';
-
+
+  insert into t_operacion_parametros
+  (
+     "ID_OPERACION"
+    ,"NOMBRE"
+    ,"VERSION"
+    ,"ORDEN"
+    ,"ACTIVO"
+    ,"TIPO_DATO"
+    ,"FORMATO"
+    ,"LONGITUD_MAXIMA"
+    ,"OBLIGATORIO"
+    ,"VALOR_DEFECTO"
+    ,"ETIQUETA"
+    ,"DETALLE"
+    ,"VALORES_POSIBLES"
+  )
+  values
+  (
+     to_number(l_varchar2(1))
+    ,to_char(l_clob(2))
+    ,to_char(l_clob(3))
+    ,to_number(l_varchar2(4))
+    ,to_char(l_clob(5))
+    ,to_char(l_clob(6))
+    ,to_char(l_clob(7))
+    ,to_number(l_varchar2(8))
+    ,to_char(l_clob(9))
+    ,to_char(l_clob(10))
+    ,to_char(l_clob(11))
+    ,to_char(l_clob(12))
+    ,to_char(l_clob(13))
+  );
+
+  l_varchar2(1) :=q'!1001!';
+  l_clob(2) :=q'!TOKEN_DISPOSITIVO!';
+  l_clob(3) :=q'!0.1.0!';
+  l_varchar2(4) :=q'!5!';
+  l_clob(5) :=q'!S!';
+  l_clob(6) :=q'!S!';
+  l_clob(7) :=q'!!';
+  l_varchar2(8) :=q'!!';
+  l_clob(9) :=q'!N!';
+  l_clob(10) :=q'!!';
+  l_clob(11) :=q'!!';
+  l_clob(12) :=q'!!';
+  l_clob(13) :=q'!!';
+
   insert into t_operacion_parametros
   (
      "ID_OPERACION"
