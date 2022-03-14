dbe8bcd6a34dc7e5da68c13a40ca2ba3137e2eba
dmezac <damian.meza.py@hotmail.com>
2022-03-13 22:47:22 -0300

    Migración 76: Ajuste en Estado de Juego en servicios de Predicciones.-

---
+++ source/database/migrations/mig_000076/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000076', 'Ajuste en Estado de Juego en servicios de Predicciones.');
+
+prompt Done.
