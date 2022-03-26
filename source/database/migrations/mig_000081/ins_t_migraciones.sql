eb4f58194a054784171401a0478db7977bc08cbd
dmezac <damian.meza.py@hotmail.com>
2022-03-26 18:47:53 -0300

    Migración 81: Ajuste en guardado y eliminación de escudos de un partido.-

---
+++ source/database/migrations/mig_000081/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000081', 'Ajuste en guardado y eliminación de escudos de un partido.');
+
+prompt Done.
