08cc24c0bfc5e2cf1605bb9a237627b669453e68
dmezac <damian.meza.py@hotmail.com>
2022-04-15 19:36:07 -0400

    Migración 85: Notificaciones personalizadas de divisiones de torneos.-

---
+++ source/database/migrations/mig_000085/ins_t_operacion_parametros.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_operacion_parametros...
+set feedback off
+set define off
+insert into t_operacion_parametros (ID_OPERACION, NOMBRE, VERSION, ORDEN, ACTIVO, TIPO_DATO, FORMATO, LONGITUD_MAXIMA, OBLIGATORIO, VALOR_DEFECTO, ETIQUETA, DETALLE, VALORES_POSIBLES)
+values (73, 'ID_DIVISION', '0.2.3', 1, 'S', 'S', null, 3, 'N', null, null, null, null);
+
+prompt Done.
--- source/database/scripts/operations/servicio/fan/listar_divisiones.sql
+++ source/database/scripts/operations/servicio/fan/listar_divisiones.sql
@@ -62,6 +62,53 @@ begin
   -----------------------------------
 
   l_varchar2(1) :=q'!73!';
+  l_clob(2) :=q'!ID_DIVISION!';
+  l_clob(3) :=q'!0.1.0!';
+  l_varchar2(4) :=q'!1!';
+  l_clob(5) :=q'!S!';
+  l_clob(6) :=q'!S!';
+  l_clob(7) :=q'!!';
+  l_varchar2(8) :=q'!3!';
+  l_clob(9) :=q'!N!';
+  l_clob(10) :=q'!!';
+  l_clob(11) :=q'!!';
+  l_clob(12) :=q'!!';
+  l_clob(13) :=q'!!';
+
+  insert into t_operacion_parametros
+  (
+     "ID_OPERACION"
+    ,"NOMBRE"
+    ,"VERSION"
+    ,"ORDEN"
+    ,"ACTIVO"
+    ,"TIPO_DATO"
+    ,"FORMATO"
+    ,"LONGITUD_MAXIMA"
+    ,"OBLIGATORIO"
+    ,"VALOR_DEFECTO"
+    ,"ETIQUETA"
+    ,"DETALLE"
+    ,"VALORES_POSIBLES"
+  )
+  values
+  (
+     to_number(l_varchar2(1))
+    ,to_char(l_clob(2))
+    ,to_char(l_clob(3))
+    ,to_number(l_varchar2(4))
+    ,to_char(l_clob(5))
+    ,to_char(l_clob(6))
+    ,to_char(l_clob(7))
+    ,to_number(l_varchar2(8))
+    ,to_char(l_clob(9))
+    ,to_char(l_clob(10))
+    ,to_char(l_clob(11))
+    ,to_char(l_clob(12))
+    ,to_char(l_clob(13))
+  );
+
+  l_varchar2(1) :=q'!73!';
   l_clob(2) :=q'!PAGINA_PARAMETROS!';
   l_clob(3) :=q'!0.1.0!';
   l_varchar2(4) :=q'!10!';
