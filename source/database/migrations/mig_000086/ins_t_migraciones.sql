f8d969c95610312898fcfd703078297e9c525e3d
dmezac <damian.meza.py@hotmail.com>
2022-04-16 19:09:57 -0400

    Migración 86: Ajuste en suscripción a notificaciones de divisiones de torneos.-

---
+++ source/database/migrations/mig_000086/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000086', 'Ajuste en suscripción a notificaciones de divisiones de torneos.');
+
+prompt Done.
