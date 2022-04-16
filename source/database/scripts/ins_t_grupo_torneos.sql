3bba7d9b39e7a9a26219942f7471eb7a20e5fb67
dmezac <damian.meza.py@hotmail.com>
2022-04-16 19:22:29 -0400

    Script pendiente ins_t_grupo_torneos.-

---
+++ source/database/scripts/ins_t_grupo_torneos.sql
@@ -0,0 +1,55 @@
+prompt Importing table t_grupo_torneos...
+set feedback off
+set define off
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (1, 'PRI-APE20', null, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (2, 'PRI-APE20', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (3, 'PRI-APE20', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (4, 'PRI-APE20', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (5, 'PRI-APE20', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (6, 'PRI-APE20', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (7, 'PRI-APE20', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (8, 'PRI-APE20', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (9, 'PRI-APE20', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (10, 'PRI-APE20', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (11, 'PRI-APE20', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (12, 'PRI-APE20', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (13, 'PRI-APE20', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (14, 'PRI-APE20', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (15, 'PRI-DEMO', null, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (16, 'PRI-DEMO', 1, null);
+
+insert into t_grupo_torneos (ID_GRUPO, ID_TORNEO, ID_JORNADA_INICIO, ID_JORNADA_FIN)
+values (17, 'PRI-DEMO', 1, null);
+
+prompt Done.
