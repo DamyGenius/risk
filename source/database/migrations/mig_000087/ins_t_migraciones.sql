c72546cedac14e8ff4c5473e80af2105a7c98508
dmezac <damian.meza.py@hotmail.com>
2022-04-17 18:49:21 -0400

    Migración 87: Ajuste en importación de partidos para torneos sin cobertura de importación.-

---
+++ source/database/migrations/mig_000087/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000087', 'Ajuste en importación de partidos para torneos sin cobertura de importación.');
+
+prompt Done.
