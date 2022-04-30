ccbd0f7adcf3add886c7a55d158a98cc26530246
dmezac <damian.meza.py@hotmail.com>
2022-04-30 11:16:32 -0400

    Migración 94: Parámetro id_torneo en servicio listar_torneos.-

---
+++ source/database/migrations/mig_000094/ins_t_operacion_parametros.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_operacion_parametros...
+set feedback off
+set define off
+insert into t_operacion_parametros (ID_OPERACION, NOMBRE, VERSION, ORDEN, ACTIVO, TIPO_DATO, FORMATO, LONGITUD_MAXIMA, OBLIGATORIO, VALOR_DEFECTO, ETIQUETA, DETALLE, VALORES_POSIBLES)
+values (74, 'ID_TORNEO', '0.3.1', 2, 'S', 'S', null, null, 'N', null, null, null, null);
+
+prompt Done.
---
+++ source/database/migrations/mig_000094/upd_t_operacion_parametros.sql
@@ -0,0 +1,4 @@
+UPDATE t_operacion_parametros a
+   SET a.orden = 3
+ WHERE a.nombre = 'SIGUIENDO'
+   AND a.id_operacion = 74;
