34643c4f484632af2cbe29db246a198d9179be9a
dmezac <damian.meza.py@hotmail.com>
2022-05-02 09:23:49 -0400

    Migración 98: Listar clubes según la división actual.-

---
+++ source/database/migrations/mig_000098/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000098', 'Listar clubes según la división actual.');
+
+prompt Done.
