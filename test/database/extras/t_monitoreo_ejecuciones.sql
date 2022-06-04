c3afa1e07ac7e7ff5abbcb2683cdb6d2cf6a910f
dmezac <damian.meza.py@hotmail.com>
2022-06-04 15:23:04 -0400

    Migración 108: Usuario resposable del monitoreo.-

--- test/database/extras/t_monitoreo_ejecuciones.sql
+++ test/database/extras/t_monitoreo_ejecuciones.sql
@@ -2,10 +2,11 @@ SELECT l.id_monitoreo_ejecucion id_ejecucion,
        l.id_monitoreo,
        --s.nombre,
        s.detalle,
-       --l.fecha_ejecucion,
+       CAST(l.fecha_ejecucion at TIME ZONE 'America/Asuncion' AS DATE) fecha,
        dbms_lob.substr(l.datos, 4000) datos
   FROM t_monitoreo_ejecuciones l, t_operaciones s
  WHERE l.id_monitoreo = s.id_operacion
-   AND l.id_monitoreo_ejecucion = &id_ejecucion
+   AND l.id_monitoreo_ejecucion =
+       nvl(to_number('&id_ejecucion'), l.id_monitoreo_ejecucion)
    AND l.id_monitoreo = nvl('&id_monitoreo', l.id_monitoreo)
  ORDER BY l.id_monitoreo_ejecucion DESC, l.id_monitoreo ASC;
