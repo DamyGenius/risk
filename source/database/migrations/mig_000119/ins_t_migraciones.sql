003dbe291b2e0c121af9ed654ebb3aeb44a7edb2
dmezac <damian.meza.py@hotmail.com>
2022-07-17 19:32:00 -0400

    Migración 119: Retornar título de jornadas en servicios de listar jornadas y listar fases.-

---
+++ source/database/migrations/mig_000119/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000119', 'Retornar título de jornadas en servicios de listar jornadas y listar fases.');
+
+prompt Done.
