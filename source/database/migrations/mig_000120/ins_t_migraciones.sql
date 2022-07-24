df94406acfae082b22a4192298e258775634bd3b
dmezac <damian.meza.py@hotmail.com>
2022-07-23 22:03:20 -0400

    Migración 120: Reprogramación de partido iniciado y suspendido.-

---
+++ source/database/migrations/mig_000120/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000120', 'Reprogramación de partido iniciado y suspendido.');
+
+prompt Done.
