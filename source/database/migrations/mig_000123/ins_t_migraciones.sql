4ba6ff38c59b017e11f77df6e1b09de6356268d3
dmezac <damian.meza.py@hotmail.com>
2022-10-16 10:05:38 -0300

    Migración 123: Ajustes en importación de partidos postergados.-

---
+++ source/database/migrations/mig_000123/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000123', 'Ajustes en importación de partidos postergados.');
+
+prompt Done.
