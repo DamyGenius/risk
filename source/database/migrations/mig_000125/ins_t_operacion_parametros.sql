5a9aac3acdb742f6a1de1c6eb94cabf2b1bc72bc
dmezac <damian.meza.py@hotmail.com>
2022-10-23 15:37:38 -0300

    Migración 125: Listar divisiones si suscripto y siguiendo.-

---
+++ source/database/migrations/mig_000125/ins_t_operacion_parametros.sql
@@ -0,0 +1,13 @@
+﻿prompt Importing table t_operacion_parametros...
+set feedback off
+set define off
+insert into t_operacion_parametros (ID_OPERACION, NOMBRE, VERSION, ORDEN, ACTIVO, TIPO_DATO, FORMATO, LONGITUD_MAXIMA, OBLIGATORIO, VALOR_DEFECTO, ETIQUETA, DETALLE, VALORES_POSIBLES)
+values (73, 'SIGUIENDO', '0.3.1', 2, 'S', 'S', null, 1, 'N', null, null, null, null);
+
+insert into t_operacion_parametros (ID_OPERACION, NOMBRE, VERSION, ORDEN, ACTIVO, TIPO_DATO, FORMATO, LONGITUD_MAXIMA, OBLIGATORIO, VALOR_DEFECTO, ETIQUETA, DETALLE, VALORES_POSIBLES)
+values (73, 'SUSCRIPTO', '0.3.1', 3, 'S', 'S', null, 1, 'N', null, null, null, null);
+
+insert into t_operacion_parametros (ID_OPERACION, NOMBRE, VERSION, ORDEN, ACTIVO, TIPO_DATO, FORMATO, LONGITUD_MAXIMA, OBLIGATORIO, VALOR_DEFECTO, ETIQUETA, DETALLE, VALORES_POSIBLES)
+values (74, 'SUSCRIPTO', '0.3.9', 4, 'S', 'S', null, 1, 'N', null, null, null, null);
+
+prompt Done.
