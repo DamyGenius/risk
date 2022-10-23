fca7df90097acf608305e8276e9dec86eb5a3878
dmezac <damian.meza.py@hotmail.com>
2022-10-23 15:10:21 -0300

    Migración 124: Recibir como parámetro al listar clubes el código ISO Alpha 2 del país.-

---
+++ source/database/migrations/mig_000124/ins_t_operacion_parametros.sql
@@ -0,0 +1,7 @@
+﻿prompt Importing table t_operacion_parametros...
+set feedback off
+set define off
+insert into t_operacion_parametros (ID_OPERACION, NOMBRE, VERSION, ORDEN, ACTIVO, TIPO_DATO, FORMATO, LONGITUD_MAXIMA, OBLIGATORIO, VALOR_DEFECTO, ETIQUETA, DETALLE, VALORES_POSIBLES)
+values (40, 'ID_PAIS_ISO2', '0.4.5', 5, 'S', 'S', null, 2, 'N', null, null, null, null);
+
+prompt Done.
--- source/database/scripts/operations/servicio/fan/listar_clubes.sql
+++ source/database/scripts/operations/servicio/fan/listar_clubes.sql
@@ -249,6 +249,53 @@ begin
     ,to_char(l_clob(13))
   );
 
+  l_varchar2(1) :=q'!40!';
+  l_clob(2) :=q'!ID_PAIS_ISO2!';
+  l_clob(3) :=q'!0.1.0!';
+  l_varchar2(4) :=q'!5!';
+  l_clob(5) :=q'!S!';
+  l_clob(6) :=q'!S!';
+  l_clob(7) :=q'!!';
+  l_varchar2(8) :=q'!2!';
+  l_clob(9) :=q'!N!';
+  l_clob(10) :=q'!!';
+  l_clob(11) :=q'!!';
+  l_clob(12) :=q'!!';
+  l_clob(13) :=q'!!';
+
+  insert into t_operacion_parametros
+  (
+     "ID_OPERACION"
+    ,"NOMBRE"
+    ,"VERSION"
+    ,"ORDEN"
+    ,"ACTIVO"
+    ,"TIPO_DATO"
+    ,"FORMATO"
+    ,"LONGITUD_MAXIMA"
+    ,"OBLIGATORIO"
+    ,"VALOR_DEFECTO"
+    ,"ETIQUETA"
+    ,"DETALLE"
+    ,"VALORES_POSIBLES"
+  )
+  values
+  (
+     to_number(l_varchar2(1))
+    ,to_char(l_clob(2))
+    ,to_char(l_clob(3))
+    ,to_number(l_varchar2(4))
+    ,to_char(l_clob(5))
+    ,to_char(l_clob(6))
+    ,to_char(l_clob(7))
+    ,to_number(l_varchar2(8))
+    ,to_char(l_clob(9))
+    ,to_char(l_clob(10))
+    ,to_char(l_clob(11))
+    ,to_char(l_clob(12))
+    ,to_char(l_clob(13))
+  );
+
 end;
 /
 /* ==================== T_SERVICIOS ==================== */
@@ -308,6 +355,21 @@ begin
 end;
 /
 /* ==================== T_TRABAJOS ==================== */
+set define off
+declare
+  type   t_clob is table of clob index by binary_integer;
+  l_clob t_clob;
+  type   t_varchar2 is table of varchar2(64) index by binary_integer;
+  l_varchar2 t_varchar2;
+begin
+
+  null;
+  -- start generation of records
+  -----------------------------------
+
+end;
+/
+/* ==================== T_MONITOREOS ==================== */
 set define off
 declare
   type   t_clob is table of clob index by binary_integer;
