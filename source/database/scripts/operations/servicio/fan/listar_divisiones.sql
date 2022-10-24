d0a8794d0b4a4af5f63e77df2259b64c7121cd3b
dmezac <damian.meza.py@hotmail.com>
2022-10-24 00:33:35 -0300

    Migración 126: Fix al listar divisiones si suscripto y siguiendo.-

--- source/database/scripts/operations/servicio/fan/listar_divisiones.sql
+++ source/database/scripts/operations/servicio/fan/listar_divisiones.sql
@@ -276,9 +276,9 @@ begin
        k_archivo.f_version_archivo('T_DIVISIONES', 'LOGO', a.id_division) version_logo,
        (SELECT decode(nvl(COUNT(1), 0), 0, 'N', 'S')
           FROM t_usuario_divisiones x
-         WHERE x.id_usuario = 21
+         WHERE x.id_usuario = k_sistema.f_id_usuario
            AND x.id_division = a.id_division) siguiendo,
-       k_usuario.f_suscripto_notificacion(21,
+       k_usuario.f_suscripto_notificacion(k_sistema.f_id_usuario,
                                           k_dispositivo.f_suscripcion_division(a.id_division)) suscripto,
        -- Torneo Actual
        b.id_torneo,
@@ -292,7 +292,7 @@ begin
          WHERE y.id_grupo =
                k_puntajes_fan.f_grupo_general_torneo(b.id_torneo)
            AND y.id_torneo = b.id_torneo
-           AND y.id_usuario = 21) ranking
+           AND y.id_usuario = k_sistema.f_id_usuario) ranking
   FROM t_divisiones a, t_torneos b
  WHERE a.id_division = b.id_division
    AND b.actual = 'S'
