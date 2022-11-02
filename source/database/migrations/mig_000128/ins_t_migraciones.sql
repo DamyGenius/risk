e16ec8d5b19526a18c60be51e4be7438ff68df7d
dmezac <damian.meza.py@hotmail.com>
2022-11-01 23:06:12 -0300

    Migración 128: 'Grupo de monitoreo de errores.-

---
+++ source/database/migrations/mig_000128/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000128', 'Grupo de monitoreo de errores.');
+
+prompt Done.
