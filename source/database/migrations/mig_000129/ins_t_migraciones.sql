f185f67a84b420cbeb1eba57e394cca46ee80c0b
dmezac <damian.meza.py@hotmail.com>
2022-11-01 23:43:14 -0300

    Migración 129: Retornar tipo de torneo en listado de fases.-

---
+++ source/database/migrations/mig_000129/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000129', 'Retornar tipo de torneo en listado de fases.');
+
+prompt Done.
