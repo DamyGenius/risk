ad2708cafe23d4f7c66e33a7ecca3fbdcba83d68
dmezac <damian.meza.py@hotmail.com>
2022-11-20 16:52:07 -0300

    Migración 134: Fix en actualización de puntajes de grupos.-

---
+++ source/database/migrations/mig_000134/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000134', 'Fix en actualización de puntajes de grupos.');
+
+prompt Done.
