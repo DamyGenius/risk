cdc02043b86ff8c0646372e24eff01f96bec6471
dmezac <damian.meza.py@hotmail.com>
2022-11-27 22:07:43 -0300

    Migración 136: Mejoras en servicios de listar grupos.-

---
+++ source/database/migrations/mig_000136/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000136', '.');
+
+prompt Done.
---
+++ source/database/migrations/mig_000136/ins_t_operacion_parametros.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_operacion_parametros...
+set feedback off
+set define off
+insert into t_operacion_parametros (ID_OPERACION, NOMBRE, VERSION, ORDEN, ACTIVO, TIPO_DATO, FORMATO, LONGITUD_MAXIMA, OBLIGATORIO, VALOR_DEFECTO, ETIQUETA, DETALLE, VALORES_POSIBLES)
+values (49, 'TORNEOS_TODOS', '0.1.0', 6, 'S', 'S', null, null, 'N', null, null, null, null);
+
+prompt Done.
