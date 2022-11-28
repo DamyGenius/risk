f0774a619b0cd5d7bacf5b3dc9acf6e53eccb60b
dmezac <damian.meza.py@hotmail.com>
2022-11-27 23:33:45 -0300

    Migración 137: Invitar a usuarios de otro grupo al crear uno nuevo.-

--- source/database/migrations/mig_000137/ins_t_migraciones.sql
+++ source/database/migrations/mig_000137/ins_t_migraciones.sql
@@ -2,6 +2,6 @@ prompt Importing table t_migraciones...
 set feedback off
 set define off
 insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
-values ('mig_000136', 'Mejoras en servicios de listar grupos.');
+values ('mig_000137', 'Invitar a usuarios de otro grupo al crear uno nuevo.');
 
 prompt Done.
