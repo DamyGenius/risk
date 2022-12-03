d5e9513e767add110360fe10ebb40ccaf14f88e6
dmezac <damian.meza.py@hotmail.com>
2022-12-03 14:00:41 -0300

    Migración 138: Fase inicial y final de los grupos (2da parte).

---
+++ source/database/migrations/mig_000138/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000138', 'Fase inicial y final de los grupos (2da parte).');
+
+prompt Done.
