fc84b5ddd5c6d9fde493560d7678898f41df001e
dmezac <damian.meza.py@hotmail.com>
2026-07-07 21:39:28 -0300

    Migración 157: Ocultar el resultado mas elegido en la notificación previa al partido.-

---
+++ source/database/migrations/mig_000157/ins_t_migraciones.sql
@@ -0,0 +1,7 @@
+prompt Importing table t_migraciones...
+set feedback off
+set define off
+insert into t_migraciones (ID_MIGRACION, DESCRIPCION)
+values ('mig_000157', 'Ocultar el resultado mas elegido en la notificación previa al partido.');
+
+prompt Done.
